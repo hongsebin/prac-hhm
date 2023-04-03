63bb46256b95cd05176d04f2e03e9695233d2dea
hongsebin <hongsb5837@gmail.com>
2023-04-03 15:15:36 +0900

    frontend antdesign css install

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,10 +1,10 @@
-import React, {useEffect, useeffect} from 'react'
+import React, {useEffect} from 'react'
 import axios from 'axios';
 
 function LandingPage() {
 
   useEffect(() => {
-    axios.get('http://localhost:3000/api/hello')
+    axios.get('/api/hello')
     // server/index.js에서 보낸걸 받는..
     .then(response => console.log(response.data))
   }, [])
